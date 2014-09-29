b02d22de16057a03b05f26fc8e250e17ccf962ea
Brendan Sheehy <bsheehy@boomboxinc.com>
2014-09-29 16:18:57 -0400

    testing callbacks

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 
+// var http = require("http");
+
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(8888);
+
 var http = require("http");
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
+  response.write("Hello World2");
   response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
